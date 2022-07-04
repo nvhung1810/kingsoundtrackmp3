8873fd1a02a71ff372af20b852d12bfa3f9e731d
nvhung1810 <coannhonbeo@gmail.com>
2022-07-04 12:42:35 +0700

    a

--- test/main.js
+++
@@ -1,5 +0,0 @@
-function hiddenMainLeft() {
-    var hidden = document.getElementById('hidden-main-left');
-    if (hidden.style.display === 'none') hidden.style.display = 'block';
-	else hidden.style.display = 'none';
-}
